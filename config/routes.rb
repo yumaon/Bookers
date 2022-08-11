ef2cf742413ae4937672755f8ec79f6c04a7e886
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-11 03:01:57 +0000

    [Add]CRUDの実装まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  post 'books'=>'books#create'
+  get 'books'=>'books#index'
+  get 'books/:id'=>'books#show',as:'book'
+  get 'books/:id/edit'=>'books#edit',as:'edit_book'
+  patch 'books/:id'=>'books#update',as:'update_book'
+  delete 'books/:id'=>'books#destroy',as:'destroy_book'
+  get root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
