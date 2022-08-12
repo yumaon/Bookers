312c01c5fab0965b1566656fac58378a5d5d591b
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-12 11:01:10 +0000

    [Add]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    if book.save
+    @book=Book.new(book_params)
+    if @book.save
       flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
     end
-    redirect_to book_path(book.id)
+
   end
 
   def index
@@ -24,19 +28,23 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
-    redirect_to book_path(book.id)
+
   end
 
   def destroy
     book=Book.find(params[:id])
     if book.destroy
       flash[:notice]="Book was successfully destroyed."
+      redirect_to '/books'
     end
-    redirect_to '/books'
+
   end
 
   private
